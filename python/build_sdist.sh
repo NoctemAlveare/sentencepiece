b673d62b36356c3616b6ef41c19597abc97d17e9
Taku Kudo <taku@google.com>
2026-05-12 08:17:32 +0000

    uses absel module on windows. include expoerts.txt in sdist

--- python/build_sdist.sh
+++ python/build_sdist.sh
@@ -2,10 +2,10 @@
 
 mkdir -p sentencepiece
 
-for i in CMakeLists.txt exports.txt LICENSE README.md VERSION.txt cmake config.h.in sentencepiece.pc.in src third_party
+for i in CMakeLists.txt LICENSE README.md VERSION.txt cmake config.h.in sentencepiece.pc.in src third_party
 do
   echo "copying ../${i} sentencepiece/${i}"
   cp -f -R "../${i}" sentencepiece
 done
 
-python -m build --sdist
+python3 -m build --sdist
